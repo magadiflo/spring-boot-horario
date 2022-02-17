249aa160c9240daa87c535cb24a326b15cc5665d
Martín <magadiflo@gmail.com>
2022-02-16 19:56:33 -0500

    Revisando error NullPointerException

--- src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/interceptors/HorarioInterceptor.java
+++ src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/interceptors/HorarioInterceptor.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -45,7 +46,9 @@ public class HorarioInterceptor implements HandlerInterceptor {
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 		String mensaje = (String) request.getAttribute("mensaje");
-		modelAndView.addObject("horario", mensaje);
+		if(modelAndView != null && handler instanceof HandlerMethod) {
+			modelAndView.addObject("horario", mensaje);			
+		}
 	}
 	
 	
